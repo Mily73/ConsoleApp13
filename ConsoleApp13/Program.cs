60bf258f9c774060f2268980d85f447425b0dc26
Mily <petyamilyaeva0809@gmail.com>
2020-06-13 16:26:00 +0300

    first commit

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ссылка на Api запрос
+            string url = "https://api.icndb.com/jokes/random";
+            string response;
+            //Отправляем запрос на сервер для получения JSON ответа
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            //Получаем ответ от сервера
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            ValueJSON valueJSON = JsonConvert.DeserializeObject<ValueJSON>(response);
+            Console.WriteLine("Шутка: {0}", valueJSON.Value.Joke);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp13/ВД50_1_18.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class ВД50_1_18
+    {
+        public string Joke { get; set; }
+    }
+}
